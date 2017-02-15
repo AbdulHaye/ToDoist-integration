9a85076c962758d1c66d27b52d08a8443f82ac4e
AbdulHaye <abdulhaye111@gmail.com>
2017-02-15 20:23:29 +0500

    Getting ID's
    Got ID's from URL
    And got ids from response after creation of task but not for the
    project.
    If exist then found in array
    if not then create it or if yes then update it.

--- AddTaskTest.php
+++ AddTaskTest.php
@@ -11,8 +11,6 @@
  *
  * @author Cresenttech
  */
-
-
 class AddTaskTest {
 
     //put your code here
@@ -26,13 +24,13 @@ class AddTaskTest {
 //              Worked For Creating task and "Project4 is created by this
 //            '[{"type": "project_add", "temp_id": "4ff1e388-5ca6-453a-b0e8-662ebf373b6b",'		8-4-4-4-12
 //            . ' "uuid": "32774db9-a1da-4550-8d9d-910372124fa4", "args": {"name": "Project4"}}]'
-            '[{"type": "item_add", "temp_id": "43f7cd23-a038-46b5-b2c9-4abda9097ffa",'
-            . ' "uuid": "997c4b43-55f1-48a9-9e66-de5785dfd69b",'
-            . ' "args": {"content": "Task2", "project_id": 196213069}}]'
+            '[{"type": "item_add", "temp_id": "43f7fs85-a068-47j5-b2c9-4ab7c90977fa",'
+            . ' "uuid": "998c4b83-5s51-48f9-3j67-de5685cfd79b",'
+            . ' "args": {"content": "Task5", "project_id": 196213069}}]'
         ];
 
         $ch = curl_init();
-        
+
         var_dump($ch);
 
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -44,10 +42,67 @@ class AddTaskTest {
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
 
         $output = curl_exec($ch);
-        
-        var_dump($output);
+
+        //var_dump($output);
 
         curl_close($ch);
+        
+        return $output;
     }
 
+    /* Xml to PHP array
+     * 
+    
+    function xmlstr_to_array($xmlstr) {
+        $doc = new DOMDocument();
+        $doc->loadXML($xmlstr);
+        $root = $doc->documentElement;
+        $output = domnode_to_array($root);
+        $output['@root'] = $root->tagName;
+        return $output;
+    }
+
+    function domnode_to_array($node) {
+        $output = array();
+        switch ($node->nodeType) {
+            case XML_CDATA_SECTION_NODE:
+            case XML_TEXT_NODE:
+                $output = trim($node->textContent);
+                break;
+            case XML_ELEMENT_NODE:
+                for ($i = 0, $m = $node->childNodes->length; $i < $m; $i++) {
+                    $child = $node->childNodes->item($i);
+                    $v = domnode_to_array($child);
+                    if (isset($child->tagName)) {
+                        $t = $child->tagName;
+                        if (!isset($output[$t])) {
+                            $output[$t] = array();
+                        }
+                        $output[$t][] = $v;
+                    } elseif ($v || $v === '0') {
+                        $output = (string) $v;
+                    }
+                }
+                if ($node->attributes->length && !is_array($output)) { //Has attributes but isn't an array
+                    $output = array('@content' => $output); //Change output into an array.
+                }
+                if (is_array($output)) {
+                    if ($node->attributes->length) {
+                        $a = array();
+                        foreach ($node->attributes as $attrName => $attrNode) {
+                            $a[$attrName] = (string) $attrNode->value;
+                        }
+                        $output['@attributes'] = $a;
+                    }
+                    foreach ($output as $t => $v) {
+                        if (is_array($v) && count($v) == 1 && $t != '@attributes') {
+                            $output[$t] = $v[0];
+                        }
+                    }
+                }
+                break;
+        }
+        return $output;
+    }
+     */
 }
--- ToDoist.php
+++ ToDoist.php
@@ -15,67 +15,67 @@ class ToDoist {
 
     private $token;
     private $url;
-    
-    //put your code here
-    public function dialUrl($username, $password, $url) {
-        if (!is_null($username) && !is_null($password) && !is_null($url)) {
-            try {
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-                curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-                $result = curl_exec($ch);
-                $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);   //get status code
-                curl_close($ch);
+    private $projects = array();
+    private $tasks = array();
+    private $team_task;
 
-                print "<pre>";
-                print_r($status_code);
-                print "</pre>";
-
-                print "<pre>";
-                print_r($result);
-                print "</pre>";
-
-                $response = array();
-                $response['status_code'] = $status_code;
-                $response['result'] = $result;
-                return $response;
-            } catch (Exception $ex) {
-                print "<pre>";
-                print_r($ex);
-                print "</pre>";
-            }
-        } else {
-            return "Missing arguements";
-        }
+    public function __construct() {
+        // Define your constructor here
     }
 
-    public function index($data) {
+    public function index() {
+
+        if (!empty($_GET)) {
+            $this->token = $_GET['token'];
+            $this->url = $_GET['url'];
+            $podio_project_id = $_GET['podio_project_id'];
+            $podio_task_name = $_GET['podio_task_name'];
+            $podio_task_id = $_GET['podio_task_id'];
+        } elseif (!empty($_POST)) {
+            $this->token = $_GET['token'];
+            $this->url = $_GET['url'];
+            $podio_project_id = $_GET['podio_project_id'];
+            $podio_task_name = $_GET['podio_task_name'];
+            $podio_task_id = $_GET['podio_task_id'];
+        }
 
         // checking url existance
-        if (array_key_exists('url', $data)) {
-            //$url = "https://todoist.com/API/v7/sync";
-            $this->url = $url;
-        } else {
-            return;
+        if (empty($this->url)) {
+            $this->url = "https://todoist.com/API/v7/sync";
         }
 
         // checking token existance
-        if (array_key_exists('token', $data)) {
-            $this->token = $data['token'];
+        if (!empty($this->token)) {
+            //do nothing
         } else {
             return;
         }
 
         //cheking project name or id existance
-        if (array_key_exists('project_id', $data)) {
-            $project_id = $data['project_id'];
-            if (array_key_exists('task_id', $data)) {
+        if (!empty($podio_project_id)) {
+            $todoist_project_id = $this->findProject($podio_project_id);
+            if (!empty($todoist_project_id) && !empty($podio_task_id)) {
+                $task_id = $this->findTask($podio_task_id);
+                if ($task_id != NULL) {
+                    try {
+                        $response = $this->updateTask($project_id, $task_id);
+                    } catch (Exception $ex) {
+                        print "<pre>";
+                        print_r($ex);
+                        print "</pre>";
+
+                        return;
+                    }
+                } else {
+                    $task_response = $this->createTask($project_id, $task_name);
+                    if (!$task_response == '37') {
+                        $this->tasks['task_name'] = $task_response;
+                    }
+                }
+            } else if (array_key_exists('project_name', $data)) {
+                $project_name = $data['project_name'];
                 try {
-                    $task_id = $data['task_id'];
-                    $response = $this->updateTask($project_id, $task_id);
+                    $project_id = $this->createProject($project_name);
                 } catch (Exception $ex) {
                     print "<pre>";
                     print_r($ex);
@@ -83,34 +83,10 @@ class ToDoist {
 
                     return;
                 }
-            } elseif (array_key_exists('task_name', $data)) {
-                try {
-                    $task_name = $data['task_name'];
-                    $task_id = $this->createTask($project_id, $task_name);
-                } catch (Exception $ex) {
-                    print "<pre>";
-                    print_r($ex);
-                    print "</pre>";
-
-                    return;
-                }
-            } else {
-                return;
             }
-        } else if (array_key_exists('project_name', $data)) {
-            $project_name = $data['project_name'];
-            try {
-                $project_id = $this->createProject($project_name);
-            } catch (Exception $ex) {
-                print "<pre>";
-                print_r($ex);
-                print "</pre>";
-
-                return;
-            }
+        }elseif(!empty ($podio_task_id) || !empty ($podio_task_name)){
+            $task_id = $this->createTask($this->team_task, $podio_task_name);
         }
-
-        
     }
 
     public function timeZoneConversion() {
@@ -145,8 +121,8 @@ class ToDoist {
             'token' => $token,
             'commands' =>
 //              Worked For Creating tasks and Projects by this script
-            '[{"type": "project_add", "temp_id": "'.$temp_id.'",'
-            . ' "uuid": "'.$uuid.'", "args": {"name": "'.$project_name.'"}}]'];
+            '[{"type": "project_add", "temp_id": "' . $temp_id . '",'
+            . ' "uuid": "' . $uuid . '", "args": {"name": "' . $project_name . '"}}]'];
 
         $ch = curl_init();
 
@@ -180,7 +156,7 @@ class ToDoist {
 //              Worked For Creating tasks and Projects by this script
             '[{"type": "item_add", "temp_id": "' . $temp_id . '",'
             . ' "uuid": "' . $uuid . '",'
-            . ' "args": {"content": "'.$task_name.'", "project_id": '.$project_id.'}}]'];
+            . ' "args": {"content": "' . $task_name . '", "project_id": ' . $project_id . '}}]'];
 
         $ch = curl_init();
 
@@ -196,16 +172,60 @@ class ToDoist {
 
         $output = curl_exec($ch);
 
-        // var_dump($output);
-        print "<pre>";
-        print_r($output);
-        print "</pre>";
-
         curl_close($ch);
+
+        $task_id = getTaskId($output, $uuid, $temp_id);
+        return $task_id;
     }
 
-    public function updateTask($project_id, $task_id) {
-        
+    public function findProject($podio_project_id) {
+        if (array_key_exists($podio_project_id, $this->projects)) {
+            return $this->projects[$podio_project_id];
+        } else {
+            return NULL;
+        }
+    }
+
+    public function findTask($podio_task_id) {
+        if (array_key_exists($podio_task_id, $this->tasks)) {
+            return $this->tasks[$podio_task_id];
+        } else {
+            return NULL;
+        }
+    }
+
+    public function updateTask($task_name) {
+        if (array_key_exists($task_name, $this->tasks)) {
+            return $this->tasks[$project_name];
+        } else {
+            return NULL;
+        }
+    }
+
+    public function getTaskId($json_obj, $uuid, $temp_id) {
+        $json = json_decode($json_obj);
+
+        foreach ($json->sync_status as $uuid_key => $mydata) {
+            // var_dump($mydata);
+            if ($uuid_key == $uuid) {
+                if ($mydata == 'ok') {
+                    foreach ($json->temp_id_mapping as $temp_id_key => $val) {
+                        if ($temp_id_key == $temp_id) {
+                            echo "App_Key : " . $val;
+                            return $val;
+                        }
+                    }
+                }
+            } else {
+
+                foreach ($mydata as $key => $val) {
+                    if ($key == "error_code") {
+                        echo "Error Code : " . $val;
+                        return $val;
+                    }
+                }
+            }
+        }
     }
 
 }
